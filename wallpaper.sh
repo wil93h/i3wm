d918d74e23e0d40b1856e2ddae298c978c3700e0
Rene_Ubuntu_League <rene93h@gmail.com>
2022-05-06 09:22:09 -0600

    I3wm conf

---
+++ wallpaper.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+<<'EOF'
+l=()
+for x in "/home/rene/backgrounds/*; do
+    if [[ $x =~ .*\.jpg$ ]] ; then
+        l+=("$x")
+    fi;
+done;
+
+if [[ "${#l[@]}" == "0" ]] ; then
+    exit
+fi
+
+wall="${l[RANDOM%${#l[@]}]}"
+
+feh --bg-fill "$wall"
+EOF
+
+feh --bg-fill --randomize /home/rene/backgrounds/*.jpg
